29a4875ac66c24747bbcbf28309e33061785af9d
Rayen Bouhachem <86076487+raiddude0@users.noreply.github.com>
2026-07-22 06:50:26 +0100

    preprocess + training done

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,8 +1,13 @@
 import re
 import pandas as pd
+from transformers import AutoTokenizer
+from src.config import MODEL_CHECKPOINT, MAX_LENGTH
+from sklearn.model_selection import train_test_split
+from datasets import Dataset
 
 
-df = pd.read_csv('../data/Tweets.csv')
+
+df = pd.read_csv('data/Tweets.csv')
 df = df[["text", "airline_sentiment"]].rename(columns={"airline_sentiment": "label"})
 
 
@@ -21,4 +26,27 @@ label2id = {"negative": 0, "neutral": 1, "positive": 2}
 id2label = {v: k for k, v in label2id.items()}
 
 df["label_id"] = df["label"].map(label2id)
-print(df.head())
+df = df[["clean_text", "label_id"]].rename(columns={"clean_text": "text", "label_id": "label"})
+
+
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
+
+train_df, temp_df = train_test_split(df, test_size=0.3, random_state=42, stratify=df["label"])
+val_df, test_df = train_test_split(temp_df, test_size=0.5, random_state=42, stratify=temp_df["label"])
+
+def tokenize_batch(batch):
+    return tokenizer(batch["text"], padding="max_length", truncation=True, max_length=MAX_LENGTH)
+
+train_dataset = Dataset.from_pandas(train_df[["text", "label"]].reset_index(drop=True))
+val_dataset = Dataset.from_pandas(val_df[["text", "label"]].reset_index(drop=True))
+test_dataset = Dataset.from_pandas(test_df[["text", "label"]].reset_index(drop=True))    
+
+train_dataset = train_dataset.map(tokenize_batch, batched=True)
+val_dataset = val_dataset.map(tokenize_batch, batched=True)
+test_dataset = test_dataset.map(tokenize_batch, batched=True)
+
+
+train_dataset.save_to_disk("data/train_dataset")
+val_dataset.save_to_disk("data/val_dataset")
+test_dataset.save_to_disk("data/test_dataset")
---
+++ src/train.py
@@ -0,0 +1,69 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from src.config import MODEL_CHECKPOINT, LABEL2ID, ID2LABEL
+from datasets import load_from_disk
+import random
+import numpy as np
+import torch
+from transformers import TrainingArguments
+from transformers import DataCollatorWithPadding, Trainer
+from sklearn.metrics import accuracy_score, f1_score
+
+
+
+tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
+model = AutoModelForSequenceClassification.from_pretrained(
+    MODEL_CHECKPOINT,
+    num_labels=len(LABEL2ID),
+    id2label=ID2LABEL,
+    label2id=LABEL2ID,
+)
+
+train_dataset = load_from_disk("data/train_dataset")
+val_dataset = load_from_disk("data/val_dataset")
+
+def set_seed(seed: int = 42):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+set_seed(42)
+
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=32,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    metric_for_best_model="f1",
+    logging_dir="./logs",
+    logging_steps=50,
+    report_to="none",
+)
+
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    predictions = np.argmax(logits, axis=-1)
+    acc = accuracy_score(labels, predictions)
+    f1 = f1_score(labels, predictions, average="weighted")
+    return {"accuracy": acc, "f1": f1}
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=train_dataset,
+    eval_dataset=val_dataset,
+    data_collator=data_collator,
+    processing_class=tokenizer,
+    compute_metrics=compute_metrics,
+)
+
+
+trainer.train()
