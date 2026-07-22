4b20ab3c699ffd90c76607684f2b6dcc4b8456b3
Rayen Bouhachem <86076487+raiddude0@users.noreply.github.com>
2026-07-22 10:34:25 +0100

    training done

--- src/train.py
+++ src/train.py
@@ -1,26 +1,35 @@
+#gpu unavailable
+import os, torch
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+torch.cuda.is_available = lambda : False
+
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from src.config import MODEL_CHECKPOINT, LABEL2ID, ID2LABEL
 from datasets import load_from_disk
 import random
 import numpy as np
-import torch
+
 from transformers import TrainingArguments
 from transformers import DataCollatorWithPadding, Trainer
 from sklearn.metrics import accuracy_score, f1_score
 
 
 
+#loading the model and tokenizer
 tokenizer = AutoTokenizer.from_pretrained(MODEL_CHECKPOINT)
 model = AutoModelForSequenceClassification.from_pretrained(
     MODEL_CHECKPOINT,
     num_labels=len(LABEL2ID),
     id2label=ID2LABEL,
     label2id=LABEL2ID,
+    device_map="cpu"
+    
 )
-
+#fine-tuning the model
 train_dataset = load_from_disk("data/train_dataset")
 val_dataset = load_from_disk("data/val_dataset")
 
+#seed for reproducibility
 def set_seed(seed: int = 42):
     random.seed(seed)
     np.random.seed(seed)
@@ -29,7 +38,7 @@ def set_seed(seed: int = 42):
 
 set_seed(42)
 
-
+#hyperparameters for training
 training_args = TrainingArguments(
     output_dir="./results",
     eval_strategy="epoch",
@@ -38,14 +47,15 @@ training_args = TrainingArguments(
     per_device_train_batch_size=16,
     per_device_eval_batch_size=32,
     num_train_epochs=3,
-    weight_decay=0.01,
+    weight_decay=0.01, #L2 regularization
     load_best_model_at_end=True,
     metric_for_best_model="f1",
     logging_dir="./logs",
     logging_steps=50,
     report_to="none",
+   
 )
-
+#training loop
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 def compute_metrics(eval_pred):
@@ -63,6 +73,7 @@ trainer = Trainer(
     data_collator=data_collator,
     processing_class=tokenizer,
     compute_metrics=compute_metrics,
+    
 )
 
 
